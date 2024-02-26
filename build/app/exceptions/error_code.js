254610527ead71825679d09948d30c17103daf67
Quentin mendel <quentin.mendel1@gmail.com>
2024-02-26 10:19:02 +0100

    feat: config sementic-release package.json file

--- build/app/exceptions/error_code.js
+++
@@ -1,9 +0,0 @@
-export var ErrorCode;
-(function (ErrorCode) {
-    ErrorCode["UN_AUTHORIZED"] = "UN_AUTHORIZED";
-    ErrorCode["TECHNICAL_ERROR"] = "TECHNICAL_ERROR";
-    ErrorCode["NOT_FOUND"] = "NOT_FOUND";
-    ErrorCode["BAD_REQUEST"] = "BAD_REQUEST";
-    ErrorCode["FORBIDDEN"] = "FORBIDDEN";
-})(ErrorCode || (ErrorCode = {}));
-//# sourceMappingURL=error_code.js.map
